94bef87b4a5f7e05e5c69676397b1b0222c47cbb
claudiohap <caraujo@dragontec.net>
2024-05-08 13:23:05 -0600

    Added model to scene

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,35 +1,26 @@
-import * as THREE from 'three'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame, type ThreeElements } from '@react-three/fiber'
+import { Canvas, useThree } from '@react-three/fiber'
+import * as THREE from "three"
+import { OrbitControls } from '@react-three/drei'
+import { Model } from './Model'
 
-function Box(props: ThreeElements['mesh']) {
-  const meshRef = useRef<THREE.Mesh>(null!)
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
 
 const Render = () => {
-  return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>)
+
+    return (
+        <div className='flex justify-center'>
+            <div className='h-120 w-120'>
+                <Canvas camera={{ position: [-8, 6, 0] }}>
+                    <Model position={[0, 0, 0]} scale={4} />
+                    <ambientLight intensity={Math.PI / 2} />
+                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                    <OrbitControls />
+                    <axesHelper args={[16]} />
+                    <gridHelper />
+                </Canvas>
+            </div>
+        </div >
+    )
 }
 
 export default Render;
