e86cc694349acc5e0788374b3860a47a0065561c
Claudio Araujo <calloman123@gmail.com>
2024-05-08 22:41:31 -0600

    fix: model position

--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,26 +1,21 @@
-import { Canvas, useThree } from '@react-three/fiber'
-import * as THREE from "three"
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Model } from './Model'
 
 
 const Render = () => {
 
-    return (
-        <div className='flex justify-center'>
-            <div className='h-120 w-120'>
-                <Canvas camera={{ position: [-8, 6, 0] }}>
-                    <Model position={[0, 0, 0]} scale={4} />
-                    <ambientLight intensity={Math.PI / 2} />
-                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                    <OrbitControls />
-                    <axesHelper args={[16]} />
-                    <gridHelper />
-                </Canvas>
-            </div>
-        </div >
-    )
+  return (
+    <div className='flex justify-center'>
+      <div className='h-80 w-80'>
+        <Canvas camera={{ position: [0, 2, -8] }}>
+          <Model position={[0, -3, 0]} scale={4} />
+          <ambientLight intensity={Math.PI / 2} />
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </div >
+  )
 }
 
 export default Render;
