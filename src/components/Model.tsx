d1d21bb6ed7e8e6738b8fdfd7c049c405c943fc1
claudiohap <caraujo@dragontec.net>
2024-06-20 08:46:40 -0600

    hotfix

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,55 +1,59 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF } from "@react-three/drei";
+import React, { useRef } from "react";
 
-export function Model(props: any) {
-    const { nodes, materials } = useGLTF("/src/assets/PortafolioAvatar.gltf")
-    return (
-        <group {...props} dispose={null}>
-            <group rotation={[0, -Math.PI / 2, 0]}>
-                <group position={[-0.102, 0.5, 0.25]}>
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.front.geometry}
-                        material={nodes.front.material}
-                        position={[-0.289, 0.25, -0.25]}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.back.geometry}
-                        material={nodes.back.material}
-                        position={[0.289, 0.219, -0.25]}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.antena.geometry}
-                        material={nodes.antena.material}
-                        position={[0.512, 1.137, -0.25]}
-                        rotation={[0, 0, 1.309]}
-                    />
-                </group>
-                <group position={[0, 0.063, 0]}>
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.cuboid.geometry}
-                        material={nodes.cuboid.material}
-                        position={[-0.083, 0.052, -0.313]}
-                        rotation={[0, -0.262, 0]}
-                    />
-                    <mesh
-                        castShadow
-                        receiveShadow
-                        geometry={nodes.cuboid_1.geometry}
-                        material={nodes.cuboid_1.material}
-                        position={[-0.083, 0.052, 0.313]}
-                        rotation={[0, 0.262, 0]}
-                    />
-                </group>
-            </group>
+const Model = React.forwardRef((props: any, ref) => {
+  const { nodes, materials } = useGLTF("/src/assets/PortafolioAvatar.gltf");
+
+  return (
+    <group {...props} dispose={null} ref={ref}>
+      <group rotation={[0, -Math.PI / 0.8, 0]}>
+        <group position={[-0.102, 0.5, 0.25]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.front.geometry}
+            material={nodes.front.material}
+            position={[-0.289, 0.25, -0.25]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.back.geometry}
+            material={nodes.back.material}
+            position={[0.289, 0.219, -0.25]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.antena.geometry}
+            material={nodes.antena.material}
+            position={[0.512, 1.137, -0.25]}
+            rotation={[0, 0, 1.309]}
+          />
         </group>
-    )
-}
+        <group position={[0, 0.063, 0]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.cuboid.geometry}
+            material={nodes.cuboid.material}
+            position={[-0.083, 0.052, -0.313]}
+            rotation={[0, -0.262, 0]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.cuboid_1.geometry}
+            material={nodes.cuboid_1.material}
+            position={[-0.083, 0.052, 0.313]}
+            rotation={[0, 0.262, 0]}
+          />
+        </group>
+      </group>
+    </group>
+  );
+});
 
-useGLTF.preload("/src/assets/PortafolioAvatar.gltf")
+useGLTF.preload("/src/assets/PortafolioAvatar.gltf");
+
+export default Model;
--- src/components/Render.tsx
+++ src/components/Render.tsx
@@ -1,29 +1,47 @@
-import { Canvas } from '@react-three/fiber'
-import { AsciiRenderer, Effects, OrbitControls } from '@react-three/drei'
-import { Model } from './Model'
-import { Pixelation } from '@react-three/postprocessing'
+import { Canvas, useThree, useFrame } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model from "./Model";
+import { useEffect, useRef } from "react";
 
+const Scene = () => {
+  const modelRef = useRef();
+  const { camera } = useThree();
+
+  useEffect(() => {
+    if (modelRef.current) {
+      camera.position.set(2, 1.8, 2);
+
+      camera.lookAt(modelRef.current.position);
+    }
+  }, [camera]);
+
+  return <Model ref={modelRef} position={[0.5, 0, 0.5]} />;
+};
 
 const Render = () => {
-
-    return (
-        <div className='flex justify-center'>
-            <div className='w-280 aspect-square bg-sky-500'>
-                <Canvas camera={{ position: [-8, 4, 0] }}>
-                    <Pixelation granularity={8} />
-                    <Model position={[0, 0, 0]} scale={4} />
-                    <ambientLight intensity={Math.PI / 2} />
-                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-                    <OrbitControls />
-                    <gridHelper />
-                    <axesHelper args={
-                        [5]
-                    }/>
-                </Canvas>
-            </div>
-        </div >
-    )
-}
+  return (
+    <div className="flex justify-center">
+      <div className="w-280 aspect-square bg-sky-500">
+        <Canvas camera={{ position: [-8, 4, 0] }}>
+          <ambientLight intensity={0.5} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight
+            position={[-10, -10, -10]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <Scene />
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </div>
+  );
+};
 
 export default Render;
