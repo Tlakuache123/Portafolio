59b6186360f7b9129ead6d221e0866f6d33f7ac3
claudiohap <caraujo@dragontec.net>
2024-07-09 12:13:40 -0600

    add: Button class

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -12,7 +12,7 @@ const DarkModeButton = () => {
   return (
     <button
       onClick={handleDarkMode}
-      className="transition-all duration-200 bg-everlightbg-500 hover:bg-everlightbg-600 dark:bg-everdarkbg-500 dark:hover:bg-everdarkbg-600 p-2 rounded hover:scale-105"
+      className="transition-all duration-200 button-colors hover:scale-105"
     >
       {darkMode ? (
         <FaSun className="w-6 h-6" />
